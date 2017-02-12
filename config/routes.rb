02af6044dd408c068a81152e942e232676fa1616
gabrielchase <gchase.patron@gmail.com>
2017-02-12 15:07:55 +0800

    Added default_url_options

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "https://gchase-rails-api.herokuapp.com"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :posts
